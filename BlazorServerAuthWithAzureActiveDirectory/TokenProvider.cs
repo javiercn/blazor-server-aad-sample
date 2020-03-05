b0c03309b54d025a2a22aaca551a019998fe631c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-03-05 07:25:29 -0800

    Update TokenProvider.cs

--- BlazorServerAuthWithAzureActiveDirectory/TokenProvider.cs
+++ BlazorServerAuthWithAzureActiveDirectory/TokenProvider.cs
@@ -8,31 +8,5 @@ namespace BlazorServerAuthWithAzureActiveDirectory
     {
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
-        public string Sub { get; private set; }
-
-        internal Task SaveTokensAsync(ClaimsPrincipal principal, string accesToken, string refreshToken)
-        {
-            Sub = principal.FindFirst(ClaimTypes.NameIdentifier).Value;
-            if (Sub == null)
-            {
-                throw new InvalidOperationException();
-            }
-
-            AccessToken = accesToken;
-            RefreshToken = refreshToken;
-
-            return Task.CompletedTask;
-        }
-
-        internal string GetTokenAsync(ClaimsPrincipal user)
-        {
-            var sub = user.FindFirst(ClaimTypes.NameIdentifier).Value;
-            if (sub == Sub)
-            {
-                return AccessToken;
-            }
-
-            return null;
-        }
     }
 }
